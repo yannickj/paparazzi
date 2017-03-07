88d89f2015694092fc98899ee83f19acf0dc16e0
Murat Bronz <muratbronz@gmail.com>
2017-03-07 10:18:05 +0100

    Fault module initiation

---
+++ sw/airborne/modules/fault/fault.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) Murat BRONZ
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file "modules/fault/fault.c"
+ * @author Murat BRONZ
+ * Generates faults on the actuators
+ */
+
+#include "modules/fault/fault.h"
+
+// void fault_init() {}
+// void fault_periodic() {}
+// void fault_event() {}
+// void fault_datalink_callback() {}
+
+
---
+++ sw/airborne/modules/fault/fault.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) Murat BRONZ
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file "modules/fault/fault.h"
+ * @author Murat BRONZ
+ * Generates faults on the actuators
+ */
+
+#ifndef FAULT_H
+#define FAULT_H
+
+// extern void fault_init();
+// extern void fault_periodic();
+// extern void fault_event();
+// extern void fault_datalink_callback();
+
+#endif
+
