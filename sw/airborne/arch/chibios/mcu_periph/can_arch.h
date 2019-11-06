09fff218cd4760cb117ae7156b4de50aedfd0198
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2019-11-06 17:43:36 +0100

    [can] start CAN driver (basic)

---
+++ sw/airborne/arch/chibios/mcu_periph/can_arch.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2019 Gautier Hattenberger <gautier.hattenberger@enac.fr>
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file arch/chibios/mcu_periph/can_arch.h
+ * @ingroup chibios_arch
+ *
+ * Handling of CAN hardware for STM32 with ChibiOS.
+ */
+#ifndef MCU_PERIPH_CHIBIOS_CAN_ARCH_H
+#define MCU_PERIPH_CHIBIOS_CAN_ARCH_H
+
+#include "std.h"
+#include "hal.h"
+
+/** Init HW can driver
+ */
+extern void can_hw_init(void);
+
+/** Basic HW transfer
+ */
+extern int can_hw_transmit(uint32_t id, const uint8_t *buf, uint8_t len);
+
+#endif /* MCU_PERIPH_CHIBIOS_CAN_ARCH_H */
