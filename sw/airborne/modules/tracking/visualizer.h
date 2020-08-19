dcf968213e3b0b77660f173e3f11262fc233a9f5
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2020-08-19 17:03:32 +0200

    remove unused files

--- sw/airborne/modules/tracking/visualizer.h
+++
@@ -1,9 +0,0 @@
-#ifndef VISUALIZER_KALMAN_H
-#define VISUALIZER_KALMAN_H
-
-extern void visualizer_init();
-
-extern void visualizer_write(float tag_tracking_roll, float tag_tracking_pitch, float tag_tracking_climb, float posx, float posy, float posz, float pltpredx, float pltpredy, float pltpredz, float pltposx, float pltposy, float pltposz);
-
-
-#endif
